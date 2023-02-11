b59f11c6d1c454a51f54fa7fb34938a7a8955a0f
MSRbuddy <meghanadasari222@gmail.com>
2023-02-11 19:54:41 +0530

    UseCase-2 is added

---
+++ LL-UC2/LL-UC2/LL-UC2.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+
+namespace LL_UC2
+{
+    internal class program
+    {
+        internal class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        internal Node head;
+        internal void Add(int d)
+        {
+            Node n = new Node(d);
+            if (head == null)
+            {
+                head = n;
+            }
+            n.next = head;
+            head = n;
+        }
+        static void Main(string[] args)
+        {
+            LinkedList<int> myList = new LinkedList<int>();
+            myList.AddFirst(70);
+            myList.AddLast(30);
+            myList.AddLast(56);
+            foreach (int d in myList)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
