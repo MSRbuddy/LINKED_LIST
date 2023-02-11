6f62831bcb6226d568ad66b8517332ab2f4562c2
MSRbuddy <meghanadasari222@gmail.com>
2023-02-11 20:10:06 +0530

    UseCase-10 is added

---
+++ LL-UC10/LL-UC10/LL-UC10.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LL_UC10
+{
+   internal class Program
+   {
+        internal class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        internal Node head;
+        internal void Add(int d)
+        {
+            Node n = new Node(d);
+            if (head == null)
+            {
+                head = n;
+            }
+            n.next = head;
+            head = n;
+        }
+        static void Main(string[] args)
+        {
+            LinkedList<int> myList = new LinkedList<int>();
+            myList.AddFirst(30);
+            myList.AddLast(40);
+            myList.AddLast(56);
+            myList.AddLast(70);
+            foreach (int d in myList)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
