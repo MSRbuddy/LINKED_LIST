cf7c30bf2a1d5337a7f7e19d95670d1d40452069
MSRbuddy <meghanadasari222@gmail.com>
2023-02-11 19:50:30 +0530

    Welcome to Linked-List problem!

---
+++ LinkedList-UC1.cs
@@ -0,0 +1,28 @@
+﻿namespace LinkedList_UC1
+{
+    internal class Program
+    {
+        internal class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to LinkedList Problem! *****");
+            LinkedList<int> myList = new LinkedList<int>();
+            myList.AddFirst(56);
+            myList.AddFirst(30);
+            myList.AddFirst(70);
+            foreach (int d in myList)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
